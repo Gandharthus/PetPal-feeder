92c61a0c1b3dae3ae9727cecec3f699a9eb9fd53
master <master@EILCALAIS.FR>
2023-10-02 09:35:18 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment= 
+{
+  firebase: {
+    projectId: 'petpal-petfeeder',
+    appId: '1:52379806500:web:f8ed53a199b66a579f21b1',
+    databaseURL: 'https://petpal-petfeeder-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'petpal-petfeeder.appspot.com',
+    apiKey: 'AIzaSyCWxiH9ZmnWOjX3_cr_cGmMNkaTUsUnlBY',
+    authDomain: 'petpal-petfeeder.firebaseapp.com',
+    messagingSenderId: '52379806500',
+  },production: false};
